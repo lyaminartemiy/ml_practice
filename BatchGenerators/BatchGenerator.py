0b6e02b8b4dbaa495791cc3f4d7b2be75b3d5fd6
Lyamin <artem.lyamin3018@yandex.ru>
2023-03-06 22:00:43 +0300

    Changes in BatchGenerator.py, LogisticRegression.py

--- BatchGenerators/BatchGenerator.py
+++ BatchGenerators/BatchGenerator.py
@@ -2,6 +2,14 @@ import numpy as np
 
 
 def batch_generator(X, y, batch_size):
+    """
+
+    :param X: np.array[n_objects, k_features]
+    :param y: np.array[n_objects_target]
+    :param batch_size: batch size of data
+    Batch generator with random sampling of batch_size elements from the sample
+    """
+    np.random.seed(42)
     perm = np.random.permutation(X)
 
     epochs = int(np.floor(X.shape[0] // batch_size))
--- LinearModels/LogisticRegression.py
+++ LinearModels/LogisticRegression.py
@@ -23,22 +23,24 @@ def sigmoid(p):
 
 class LogisticRegression:
 
-    def __init__(self, batch_size=10):
-        self.batch_size = batch_size
+    def __init__(self):
         self.w = None
 
     def fit(self, X, y, lr=0.1, batch_size=10):
         n_objects = X.shape[0]
         self.w = np.array(np.random.rand(n_objects))
 
+        X = np.c_[X, np.ones(X.shape[0])]
+
         loses = []
 
         epochs = int(np.floor(X.shape[0] // batch_size))
         for i in range(0, epochs):
             for X_batch, y_batch in batch_generator(X, y, batch_size):
-                predictions = self.predict_proba(X_batch, y_batch)
 
-                loss = self.__loss(predictions)
+                predictions = self.predict_proba_internal(X_batch)
+
+                loss = self.__loss(y_batch, predictions)
                 loses.append(loss)
 
                 self.w = self.w - (lr * self.get_grad(X_batch, y_batch, predictions))
@@ -50,10 +52,18 @@ class LogisticRegression:
         grad = np.transpose(X_batch) @ (predictions - y_batch)
         return grad
 
-    @staticmethod
-    def predict_proba(X, w):
+    def predict_proba(self, X):
         X = np.c_[X, np.ones(X.shape[0])]
-        return sigmoid(logit(X, w))
+        return sigmoid(logit(X, self.w))
+
+    def predict_proba_internal(self, X):
+        return sigmoid(logit(X, self.w))
+
+    def predict(self, X, threshold=0.5):
+        return self.predict_proba(X) >= threshold
+
+    def get_weights(self):
+        return self.w.copy()
 
     @staticmethod
     def __loss(y, p):
