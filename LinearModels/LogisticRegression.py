8c7bee6c350f970759afc81d6ccc677b0578148c
Lyamin <artem.lyamin3018@yandex.ru>
2023-03-07 19:52:47 +0300

    Changes in LogisticRegression.py Delete BatchGenerator.py

--- LinearModels/LogisticRegression.py
+++ LinearModels/LogisticRegression.py
@@ -1,5 +1,23 @@
 import numpy as np
-from BatchGenerators.BatchGenerator import batch_generator
+
+
+def batch_generator(X, y, batch_size):
+    """
+
+    :param X: np.array[n_objects, k_features]
+    :param y: np.array[n_objects_target]
+    :param batch_size: batch size of data
+    Batch generator with random sampling of batch_size elements from the sample
+    """
+    np.random.seed(42)
+    perm = np.random.permutation(X)
+
+    epochs = int(np.floor(X.shape[0] // batch_size))
+    for batch_ in range(0, epochs):
+        X_batch, y_batch = X[perm][batch_:((batch_ + 1) * batch_size)], \
+                           y[perm][batch_:((batch_ + 1) * batch_size)]
+        X, y = X[perm][batch_size:], y[perm][batch_size:]
+        yield X_batch, y_batch
 
 
 def logit(X, w):
@@ -39,13 +57,12 @@ class LogisticRegression(object):
         epochs = int(np.floor(X.shape[0] // batch_size))
         for i in range(0, epochs):
             for X_batch, y_batch in batch_generator(X, y, batch_size):
-
                 predictions = self.predict_proba_internal(X_batch)
 
                 loss = self.__loss(y_batch, predictions)
                 loses.append(loss)
 
-                self.w = self.w - (lr * self.get_grad(X_batch, y_batch, predictions))
+                self.w = self.w - (lr * get_grad(X_batch, y_batch, predictions))
 
         return loses
 
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from LinearModels import LogisticRegression
 
 
 if __name__ == "__main__":
+    print(dir(LogisticRegression))
     m = LogisticRegression
     X = np.array([[1, 3, 4], [1, -5, 6], [-3, 5, 3]])
     X = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
     y = np.array([1, 0, 1])
-    preds = np.array([.55, .22, .85])
-    grads = LogisticRegression.get_grad(X, y, preds)
+    predictions = np.array([.55, .22, .85])
+    grads = m.get_grad(X, y, predictions)
     assert np.allclose(grads, np.array([-0.38, 0.22, -3.2, -0.93])), "Что-то не так!"
